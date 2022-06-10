a6993e9d670f1a6c3552c0c9e74d76bf61142c69
flvSantos15 <flvSantos300@gmail.com>
2022-06-10 19:31:10 -0300

    added eslint file

--- src/pages/signout/index.tsx
+++ src/pages/signout/index.tsx
@@ -1,6 +1,10 @@
 import { Flex, Text } from '@chakra-ui/react'
 
 export function SignOut() {
+  const test = 'testando eslist';
+  const testEslint = "configurando eslint"
+
+  console.log()
   return (
     <Flex>
       <Text>SignOut</Text>
